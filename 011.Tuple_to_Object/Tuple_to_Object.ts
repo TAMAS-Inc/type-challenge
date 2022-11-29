8bf1e548995030283388876872a04e615eab7580
codeisneverodd <codeisneverodd@gmail.com>
2022-11-29 09:30:49 +0900

    경현: 221129  풀이 완료

--- 011.Tuple_to_Object/Tuple_to_Object.ts
+++ 011.Tuple_to_Object/Tuple_to_Object.ts
@@ -20,30 +20,32 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type TupleToObject<T extends readonly any[]> = any;
+type TupleToObject<T extends readonly (string | number)[]> = {
+  [k in T[number]]: k;
+};
 
 /* _____________ 테스트 케이스 _____________ */
-import type { Equal, Expect } from '@type-challenges/utils';
+import type { Equal, Expect } from "@type-challenges/utils";
 
-const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const;
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
 const tupleNumber = [1, 2, 3, 4] as const;
-const tupleMix = [1, '2', 3, '4'] as const;
+const tupleMix = [1, "2", 3, "4"] as const;
 
 type cases = [
   Expect<
     Equal<
       TupleToObject<typeof tuple>,
       {
-        tesla: 'tesla';
-        'model 3': 'model 3';
-        'model X': 'model X';
-        'model Y': 'model Y';
+        tesla: "tesla";
+        "model 3": "model 3";
+        "model X": "model X";
+        "model Y": "model Y";
       }
     >
   >,
   Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
   Expect<
-    Equal<TupleToObject<typeof tupleMix>, { 1: 1; '2': '2'; 3: 3; '4': '4' }>
+    Equal<TupleToObject<typeof tupleMix>, { 1: 1; "2": "2"; 3: 3; "4": "4" }>
   >
 ];
 
