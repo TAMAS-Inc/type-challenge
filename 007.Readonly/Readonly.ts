b036d474afb5d7b2f85545c138affa965313836f
OrangeChick <retww@naver.com>
2022-11-29 22:05:10 +0900

    🍊JUNY: 1일차 문제풀이

--- 007.Readonly/Readonly.ts
+++ 007.Readonly/Readonly.ts
@@ -29,10 +29,10 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type MyReadonly<T> = any;
+type MyReadonly<T> = { readonly [K in keyof T]: T[K] };
 
 /* _____________ 테스트 케이스 _____________ */
-import type { Equal, Expect } from '@type-challenges/utils';
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>];
 
