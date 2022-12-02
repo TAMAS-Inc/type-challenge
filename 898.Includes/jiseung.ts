0580528231146f07c401c5ad8782848e8da0e798
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-12-02 10:45:34 +0900

    📝 4일차 풀이

---
+++ 898.Includes/jiseung.ts
@@ -0,0 +1,14 @@
+type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <B>() => B extends Y
+  ? 1
+  : 2
+  ? true
+  : false;
+
+type Includes<T extends readonly any[], U> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? Equal<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false;
