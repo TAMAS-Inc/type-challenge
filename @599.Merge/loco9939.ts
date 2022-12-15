8b4a2f6d7eab4347a70ed1cd705e21c574f641d2
loco9939 <kls9939@naver.com>
2022-12-15 09:56:08 +0900

    yiju

---
+++ @599.Merge/loco9939.ts
@@ -0,0 +1,68 @@
+/*
+  599 - Merge
+  -------
+  by ZYSzys (@ZYSzys) #medium #object
+  
+  ### Question
+  
+  Merge two types into a new type. Keys of the second type overrides keys of the first type.
+  
+  For example
+  
+  ```ts
+  type foo = {
+    name: string;
+    age: string;
+  }
+  type coo = {
+    age: number;
+    sex: string
+  }
+  
+  type Result = Merge<foo,coo>; // expected to be {name: string, age: number, sex: string}
+  ```
+  
+  > View on GitHub: https://tsch.js.org/599
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Merge<F extends { [k: string]: any }, S extends { [k: string]: any }> = {
+  [K in keyof F | keyof S]: K extends keyof S
+    ? S[K]
+    : K extends keyof F
+    ? F[K]
+    : never;
+};
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type Foo = {
+  a: number;
+  b: string;
+};
+type Bar = {
+  b: number;
+  c: boolean;
+};
+
+type cases = [
+  Expect<
+    Equal<
+      Merge<Foo, Bar>,
+      {
+        a: number;
+        b: number;
+        c: boolean;
+      }
+    >
+  >
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/599/answer
+  > View solutions: https://tsch.js.org/599/solutions
+  > More Challenges: https://tsch.js.org
+*/
