4676f2be9817279ae30cd48a9e8ac7cbc5a51bd2
codeisneverodd <codeisneverodd@gmail.com>
2022-12-16 09:34:20 +0900

    20221216 풀이완료

---
+++ @1097.IsUnion/codeisneverodd.ts
@@ -0,0 +1,55 @@
+/*
+  1097 - IsUnion
+  -------
+  by null (@bencor) #medium #union
+  
+  ### 문제
+  
+  입력 타입 'T'를 사용하고 'T'가 Union 타입으로 확인되는지 여부를 반환하는 타입 'IsUnion'을 구현합니다.
+
+  예시:
+    
+    ```ts
+    type case1 = IsUnion<string>  // false
+    type case2 = IsUnion<string|number>  // true
+    type case3 = IsUnion<[string|number]>  // false
+    ```
+  
+  > View on GitHub: https://tsch.js.org/1097
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type IsUnion<T, K = T> = [T] extends [never]
+  ? false
+  : T extends never
+  ? false
+  : [K] extends [T]
+  ? false
+  : true;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<IsUnion<string>, false>>,
+  Expect<Equal<IsUnion<string | number>, true>>,
+  Expect<Equal<IsUnion<"a" | "b" | "c" | "d">, true>>,
+  Expect<Equal<IsUnion<undefined | null | void | "">, true>>,
+  Expect<Equal<IsUnion<{ a: string } | { a: number }>, true>>,
+  Expect<Equal<IsUnion<{ a: string | number }>, false>>,
+  Expect<Equal<IsUnion<[string | number]>, false>>,
+  // Cases where T resolves to a non-union type.
+  Expect<Equal<IsUnion<string | never>, false>>,
+  Expect<Equal<IsUnion<string | unknown>, false>>,
+  Expect<Equal<IsUnion<string | any>, false>>,
+  Expect<Equal<IsUnion<string | "a">, false>>,
+  Expect<Equal<IsUnion<never>, false>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/1097/answer
+  > View solutions: https://tsch.js.org/1097/solutions
+  > More Challenges: https://tsch.js.org
+*/
