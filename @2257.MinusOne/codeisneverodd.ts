2705b571259a83022f61b928923fa9c7546ef7e7
codeisneverodd <codeisneverodd@gmail.com>
2022-12-19 09:24:30 +0900

    feat: 221219 풀이완료

---
+++ @2257.MinusOne/codeisneverodd.ts
@@ -0,0 +1,65 @@
+/*
+  2257 - MinusOne
+  -------
+  by Mustafo Faiz (@fayzzzm) #medium #math
+  
+  ### 문제
+  
+  타입으로 숫자(항상 양수)가 주어집니다. 타입은 1 감소된 숫자를 반환해야 합니다.
+
+  For example:
+  
+  ```ts
+  type Zero = MinusOne<1> // 0
+  type FiftyFour = MinusOne<55> // 54
+  ```
+  
+  > View on GitHub: https://tsch.js.org/2257
+*/
+
+/* _____________ Your Code Here _____________ */
+
+// type MinusOne<T extends number, Tuple extends unknown[] = []> = T extends 0
+//   ? -1
+//   : [unknown, ...Tuple]["length"] extends T
+//   ? Tuple["length"]
+//   : MinusOne<T, [unknown, unknown, ...Tuple]>;
+// type ParseInt<T extends string> = T extends `${infer Digit extends number}`
+//   ? Digit
+//   : never;
+// type ReverseString<S extends string> = S extends `${infer First}${infer Rest}`
+//   ? `${ReverseString<Rest>}${First}`
+//   : '';
+// type RemoveLeadingZeros<S extends string> = S extends '0'
+//   ? S
+//   : S extends `${'0'}${infer R}`
+//   ? RemoveLeadingZeros<R>
+//   : S;
+// type InternalMinusOne<S extends string> =
+//   S extends `${infer Digit extends number}${infer Rest}`
+//     ? Digit extends 0
+//       ? `9${InternalMinusOne<Rest>}`
+//       : `${[9, 0, 1, 2, 3, 4, 5, 6, 7, 8][Digit]}${Rest}`
+//     : never;
+// type MinusOne<T extends number> = T extends 0 ? -1 : ParseInt<
+//   RemoveLeadingZeros<ReverseString<InternalMinusOne<ReverseString<`${T}`>>>>
+// >;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<MinusOne<1>, 0>>,
+  Expect<Equal<MinusOne<55>, 54>>,
+  Expect<Equal<MinusOne<3>, 2>>,
+  Expect<Equal<MinusOne<100>, 99>>,
+  Expect<Equal<MinusOne<1101>, 1100>>,
+  Expect<Equal<MinusOne<0>, -1>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2257/answer
+  > View solutions: https://tsch.js.org/2257/solutions
+  > More Challenges: https://tsch.js.org
+*/
---
+++ @2595.PickByType/codeisneverodd.ts
@@ -0,0 +1,55 @@
+/*
+  2595 - PickByType
+  -------
+  by jiangshan (@jiangshanmeta) #medium #object
+  
+  ### 질문
+  
+  'T'에서 'U'에 할당할 수 있는 타입의 프로퍼티 집합을 선택합니다.
+
+  예시
+  
+  ```typescript
+  type OnlyBoolean = PickByType<{
+    name: string
+    count: number
+    isReadonly: boolean
+    isEnable: boolean
+  }, boolean> // { isReadonly: boolean; isEnable: boolean; }
+  ```
+  
+  > View on GitHub: https://tsch.js.org/2595
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type PickByType<T, U> = {
+  [K in keyof T as T[K] extends U ? K : never]: T[K];
+};
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+interface Model {
+  name: string;
+  count: number;
+  isReadonly: boolean;
+  isEnable: boolean;
+}
+
+type cases = [
+  Expect<
+    Equal<
+      PickByType<Model, boolean>,
+      { isReadonly: boolean; isEnable: boolean }
+    >
+  >,
+  Expect<Equal<PickByType<Model, string>, { name: string }>>,
+  Expect<Equal<PickByType<Model, number>, { count: number }>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2595/answer
+  > View solutions: https://tsch.js.org/2595/solutions
+  > More Challenges: https://tsch.js.org
+*/
