8505a1cd9385ba151f9923f14264341138642cd0
codeisneverodd <codeisneverodd@gmail.com>
2023-01-11 16:01:15 +0900

    feat: 230111 문제풀이

---
+++ @3196.Flip_Arguments/codeisneverodd.ts
@@ -0,0 +1,74 @@
+/*
+  3196 - Flip Arguments
+  -------
+  by jiangshan (@jiangshanmeta) #medium #arguments
+
+  ### Question
+
+  Implement the type version of lodash's ```_.flip```.
+
+  Type ```FlipArguments<T>``` requires function type ```T``` and returns a new function type which has the same return type of T but reversed parameters.
+
+  For example:
+
+  ```typescript
+  type Flipped = FlipArguments<(arg0: string, arg1: number, arg2: boolean) => void>
+  // (arg0: boolean, arg1: number, arg2: string) => void
+  ```
+
+  > View on GitHub: https://tsch.js.org/3196
+*/
+
+/* _____________ Your Code Here _____________ */
+//함수 파라미터를 받을 때, infer 활용
+type ReverseTuple<T> = T extends [...infer Heads, infer Tail]
+  ? [Tail, ...ReverseTuple<Heads>]
+  : [];
+
+type FlipArguments<T extends Function> = T extends (
+  ...args: infer TArguments
+) => infer TReturn
+  ? (...args: ReverseTuple<TArguments>) => TReturn
+  : never;
+
+// type Reverse<T extends any[]> = T extends [infer A, ...infer B]
+//   ? [...Reverse<B>, A]
+//   : T;
+
+// type FlipArguments<T extends (...args: any) => any> = (
+//   ...args: Reverse<Parameters<T>>
+// ) => ReturnType<T>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<FlipArguments<() => boolean>, () => boolean>>,
+  Expect<
+    Equal<FlipArguments<(foo: string) => number>, (foo: string) => number>
+  >,
+  Expect<
+    Equal<
+      FlipArguments<(arg0: string, arg1: number, arg2: boolean) => void>,
+      (arg0: boolean, arg1: number, arg2: string) => void
+    >
+  >
+];
+
+type errors = [
+  // @ts-expect-error
+  FlipArguments<"string">,
+  // @ts-expect-error
+  FlipArguments<{ key: "value" }>,
+  // @ts-expect-error
+  FlipArguments<["apple", "banana", 100, { a: 1 }]>,
+  // @ts-expect-error
+  FlipArguments<null | undefined>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/3196/answer
+  > View solutions: https://tsch.js.org/3196/solutions
+  > More Challenges: https://tsch.js.org
+*/
