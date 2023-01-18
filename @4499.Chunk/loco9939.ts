96facf22749528e4d3076cc1588a61dca5bea834
loco9939 <kls9939@naver.com>
2023-01-18 15:58:28 +0900

    yiju

---
+++ @4499.Chunk/loco9939.ts
@@ -0,0 +1,54 @@
+/*
+  4499 - Chunk
+  -------
+  by キリサメ qianxi (@qianxi0410) #medium #tuple
+
+  ### Question
+
+  Do you know `lodash`? `Chunk` is a very useful function in it, now let's implement it.
+  `Chunk<T, N>` accepts two required type parameters, the `T` must be a `tuple`, and the `N` must be an `integer >=1`
+
+  ```ts
+  type exp1 = Chunk<[1, 2, 3], 2> // expected to be [[1, 2], [3]]
+  type exp2 = Chunk<[1, 2, 3], 4> // expected to be [[1, 2, 3]]
+  type exp3 = Chunk<[1, 2, 3], 1> // expected to be [[1], [2], [3]]
+  ```
+
+  > View on GitHub: https://tsch.js.org/4499
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Chunk<
+  T extends unknown[],
+  N,
+  Saved extends unknown[] = []
+> = Saved["length"] extends N
+  ? [Saved, ...Chunk<T, N>]
+  : T extends [infer A, ...infer B]
+  ? B extends []
+    ? [[...Saved, A]]
+    : Chunk<B, N, [...Saved, A]>
+  : [];
+
+// 테케의 제네릭 매개변수는 2개가 들어가는데 우리가 만들어주는 제네릭의 매개변수는 왜 3개로 해주는건가?
+// default
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Chunk<[], 1>, []>>,
+  Expect<Equal<Chunk<[1, 2, 3], 1>, [[1], [2], [3]]>>,
+  Expect<Equal<Chunk<[1, 2, 3], 2>, [[1, 2], [3]]>>,
+  Expect<Equal<Chunk<[1, 2, 3, 4], 2>, [[1, 2], [3, 4]]>>,
+  Expect<Equal<Chunk<[1, 2, 3, 4], 5>, [[1, 2, 3, 4]]>>,
+  Expect<Equal<Chunk<[1, true, 2, false], 2>, [[1, true], [2, false]]>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4499/answer
+  > View solutions: https://tsch.js.org/4499/solutions
+  > More Challenges: https://tsch.js.org
+*/
