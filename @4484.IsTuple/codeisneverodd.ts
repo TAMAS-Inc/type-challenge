156890217c4abfa0f9af40bc675f40ba27afdce3
codeisneverodd <codeisneverodd@gmail.com>
2023-01-18 15:59:26 +0900

    feat: 230118 문제풀이

---
+++ @4484.IsTuple/codeisneverodd.ts
@@ -0,0 +1,54 @@
+/*
+  4484 - IsTuple
+  -------
+  by jiangshan (@jiangshanmeta) #medium #tuple
+
+  ### Question
+
+  Implement a type ```IsTuple```, which takes an input type ```T``` and returns whether ```T``` is tuple type.
+
+  For example:
+
+  ```typescript
+  type case1 = IsTuple<[number]> // true
+  type case2 = IsTuple<readonly [number]> // true
+  type case3 = IsTuple<number[]> // false
+  ```
+
+  > View on GitHub: https://tsch.js.org/4484
+*/
+
+/* _____________ Your Code Here _____________ */
+// 1. any[] 의 길이는 number로 평가된다.
+// type b = any[]['length']
+//  ^? b = number 로 추론
+// 2. Tuple은 readonly 이다.
+// 3. never는 모든 타입의 부분 집합이다. 따라서
+// type a = never extends any ? true: false
+//  ^? a = true 로 추론
+type IsTuple<T> = [T] extends [never]
+  ? false
+  : T extends readonly any[]
+  ? number extends T["length"]
+    ? false
+    : true
+  : false;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<IsTuple<[]>, true>>,
+  Expect<Equal<IsTuple<[number]>, true>>,
+  Expect<Equal<IsTuple<readonly [1]>, true>>,
+  Expect<Equal<IsTuple<{ length: 1 }>, false>>,
+  Expect<Equal<IsTuple<number[]>, false>>,
+  Expect<Equal<IsTuple<never>, false>>,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4484/answer
+  > View solutions: https://tsch.js.org/4484/solutions
+  > More Challenges: https://tsch.js.org
+*/
